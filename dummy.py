2090c37e0a8fb9fd9fd64e1eab58a0dcf6aad0e3
Amelia Backes <33259444+abackes19@users.noreply.github.com>
2019-06-19 11:55:50 -0500

    update comments

--- dummy.py
+++ dummy.py
@@ -1,5 +1,11 @@
 # dummy for code on arm
-# first of all import the socket library
+# don't need to necessarily print the messages, just helpful to know where we're at
+# will receive the data as [x, y, z]
+# the while loop is what the motor control code should be looping too
+# so its continuously receiving a connection and fresh data
+# pickles is for making legible, useable data
+# right now can run with diagram.py to print the coordinates it's supes cool
+
 import socket
 import pickle
 
@@ -15,9 +21,9 @@ print "socket is listening"
 
 c, addr = s.accept()
 print 'Got connection from', addr
-
-# send a thank you message to the client.
 c.send('Thank you for connecting')
+
+
 while True:
     xyz = c.recv(5000)
 
