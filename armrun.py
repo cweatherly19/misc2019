856d047c9043701febdbbf252240120d6bfe2800
Amelia Backes <33259444+abackes19@users.noreply.github.com>
2019-06-24 11:45:16 -0500

    UGH

--- armrun.py
+++ armrun.py
@@ -96,78 +96,75 @@ def key_reader():
     xyz = c.recv(5000)
     input = pickle.loads(xyz)
     x = int(input[0])
-    global x
     y = int(input[1])
-    global y
     z = int(input[2])
-    global z
     print input
-    print y
+    return x, y, z
    
 
-def motor_runner(): #sends signals to all the motors based on potentiometer readings
-    while quit == False:
-        reach_length = math.sqrt(x ** 2 + y ** 2 + z ** 2) #the momentary length of the arm
+def motor_runner(x, y, z): #sends signals to all the motors based on potentiometer readings
 
-        elbow_value = math.acos((d_one ** 2 + d_two ** 2 - reach_length ** 2) / (2 * d_one * d_two)) #the actual value of the elbow angle
-        a_elbow = round(abs(elbow_value - math.pi), 4) #the converted angle of the elbow
-        a_shoulder = round(math.asin(d_two * math.sin(elbow_value) / reach_length) + math.asin(y / reach_length), 4) #the shoulder angle
-        try:
-            a_swivel = round(math.asin(z / math.sqrt(x ** 2 + z ** 2)) + math.pi / 2, 4) #the swivel angle
-        except:
-            a_swivel = math.pi / 2 #the swivel angle when its angle doesn't matter
+    reach_length = math.sqrt(x ** 2 + y ** 2 + z ** 2) #the momentary length of the arm
 
-        try:
-            #move shoulder motor
-            pot_shoulder = RPL.analogRead(ppin_shoulder) * 3 * math.pi / (2 * shoulder_range) - math.pi / 4
-            error_s = abs(pot_shoulder - a_shoulder) #how many degrees off the intended value the arm is
-            calculated_error_s = error_s * d_two
-            if calculated_error_s > max_error:
-                if pot_shoulder > a_shoulder:
-                    RPL.digitalWrite(shoulder_dir, 0) #turn clockwise
-                else:
-                    RPL.digitalWrite(shoulder_dir, 1) #turn counterclockwise
-                RPL.pwmWrite(shoulder_pul, shoulder_motor_speed, shoulder_motor_speed * 2)
+    elbow_value = math.acos((d_one ** 2 + d_two ** 2 - reach_length ** 2) / (2 * d_one * d_two)) #the actual value of the elbow angle
+    a_elbow = round(abs(elbow_value - math.pi), 4) #the converted angle of the elbow
+    a_shoulder = round(math.asin(d_two * math.sin(elbow_value) / reach_length) + math.asin(y / reach_length), 4) #the shoulder angle
+    try:
+        a_swivel = round(math.asin(z / math.sqrt(x ** 2 + z ** 2)) + math.pi / 2, 4) #the swivel angle
+    except:
+        a_swivel = math.pi / 2 #the swivel angle when its angle doesn't matter
+
+    try:
+        #move shoulder motor
+        pot_shoulder = RPL.analogRead(ppin_shoulder) * 3 * math.pi / (2 * shoulder_range) - math.pi / 4
+        error_s = abs(pot_shoulder - a_shoulder) #how many degrees off the intended value the arm is
+        calculated_error_s = error_s * d_two
+        if calculated_error_s > max_error:
+            if pot_shoulder > a_shoulder:
+                RPL.digitalWrite(shoulder_dir, 0) #turn clockwise
             else:
-                RPL.pwmWrite(shoulder_pul, 0, shoulder_motor_speed * 2) #stops running while in range
-           
-            #move elbow motor
-            pot_elbow = RPL.analogRead(ppin_elbow) * 3 * math.pi / (2 * elbow_range) - math.pi / 4
-            error_e = abs(pot_elbow - a_elbow) #how many degrees off the intended value the arm is
-            calculated_error_e = error_e * d_two
-            if calculated_error_e > max_error:
-                if pot_elbow > a_elbow:
-                    RPL.digitalWrite(elbow_dir, 0) #turn clockwise
-                else:
-                    RPL.digitalWrite(elbow_dir, 1) #turn counterclockwise
-                RPL.pwmWrite(elbow_pul, elbow_motor_speed, elbow_motor_speed * 2)
-            else:
-                RPL.pwmWrite(elbow_pul, 0, elbow_motor_speed * 2) #stops running while in range
-          
-            #move swivel motor
-            pot_swivel = RPL.analogRead(ppin_swivel) * 3 * math.pi / (2 * swivel_range) - math.pi / 4
-            error_sw = abs(pot_swivel - a_swivel) #how many degrees off the intended value the arm is
-            if error_sw > max_error:
-                if pot_swivel > a_swivel:
-                    RPL.servoWrite(swivel_continuous, 2000) #turn clockwise
-                else:
-                    RPL.servoWrite(swivel_continuous, 1000) #turn counterclockwise
-            else:
-                RPL.servoWrite(swivel_continuous, 0) #stops running while in range
+                RPL.digitalWrite(shoulder_dir, 1) #turn counterclockwise
+            RPL.pwmWrite(shoulder_pul, shoulder_motor_speed, shoulder_motor_speed * 2)
+        else:
+            RPL.pwmWrite(shoulder_pul, 0, shoulder_motor_speed * 2) #stops running while in range
 
-            if quit == True: #stop the motors when the code ends
-                RPL.servoWrite(swivel_continuous, 0) #stops running while in range
-                RPL.pwmWrite(elbow_pul, 0, elbow_motor_speed * 2) #stops running while in range
-                RPL.pwmWrite(shoulder_pul, 0, shoulder_motor_speed * 2) #stops running while in range
+        #move elbow motor
+        pot_elbow = RPL.analogRead(ppin_elbow) * 3 * math.pi / (2 * elbow_range) - math.pi / 4
+        error_e = abs(pot_elbow - a_elbow) #how many degrees off the intended value the arm is
+        calculated_error_e = error_e * d_two
+        if calculated_error_e > max_error:
+            if pot_elbow > a_elbow:
+                RPL.digitalWrite(elbow_dir, 0) #turn clockwise
+            else:
+                RPL.digitalWrite(elbow_dir, 1) #turn counterclockwise
+            RPL.pwmWrite(elbow_pul, elbow_motor_speed, elbow_motor_speed * 2)
+        else:
+            RPL.pwmWrite(elbow_pul, 0, elbow_motor_speed * 2) #stops running while in range
 
-        except: #to show the values of the motor arm
-            
-            print('[elbow, shoulder, swivel]:', [round(a_elbow, 4), round(a_shoulder, 4), round(a_swivel, 4)], '[Speed]:', [speed], '[x, y, z]:', [round(x, 2), round(y, 2), round(z, 2)])
+        #move swivel motor
+        pot_swivel = RPL.analogRead(ppin_swivel) * 3 * math.pi / (2 * swivel_range) - math.pi / 4
+        error_sw = abs(pot_swivel - a_swivel) #how many degrees off the intended value the arm is
+        if error_sw > max_error:
+            if pot_swivel > a_swivel:
+                RPL.servoWrite(swivel_continuous, 2000) #turn clockwise
+            else:
+                RPL.servoWrite(swivel_continuous, 1000) #turn counterclockwise
+        else:
+            RPL.servoWrite(swivel_continuous, 0) #stops running while in range
+
+        if quit == True: #stop the motors when the code ends
+            RPL.servoWrite(swivel_continuous, 0) #stops running while in range
+            RPL.pwmWrite(elbow_pul, 0, elbow_motor_speed * 2) #stops running while in range
+            RPL.pwmWrite(shoulder_pul, 0, shoulder_motor_speed * 2) #stops running while in range
+
+    except: #to show the values of the motor arm
+
+        print('[elbow, shoulder, swivel]:', [round(a_elbow, 4), round(a_shoulder, 4), round(a_swivel, 4)], '[Speed]:', [speed], '[x, y, z]:', [round(x, 2), round(y, 2), round(z, 2)])
 
 while True:
     try:
-        key_reader()
-        motor_runner()
+        x, y, z = key_reader()
+        motor_runner(x, y, z)
     except:
         break
             
