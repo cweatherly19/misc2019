39e89f0941513e5be9b0083c503c97becd7e164e
Amelia Backes <33259444+abackes19@users.noreply.github.com>
2019-07-03 12:59:17 +1000

    Add files via upload

---
+++ killclient.py
@@ -0,0 +1,24 @@
+import socket
+import pickle
+
+# Create a socket object
+s = socket.socket()
+
+oof = [0, 0]
+
+# Define the port on which you want to connect
+port = 7777
+
+# connect to the server on local computer
+# first number if on computer: 127.0.0.1
+# if on pi: 192.168.21.xxx, xxx being the chip #
+s.connect(('192.168.1.2', port))
+
+data = pickle.dumps(oof)	#Pickles the data [see ControlPyEvolvedCore for more information]
+s.sendall(data)
+
+
+# receive data from the server
+print s.recv(200)
+# close the connection
+s.close()
---
+++ killserver.py
@@ -0,0 +1,28 @@
+import socket
+import pickle
+import RoboPiLib as RPL
+import setup
+
+s = socket.socket()
+port = 7777
+s.bind(('', port))
+s.listen(5)
+c, addr = s.accept()
+
+print 'Got connection from', addr
+
+# send a thank you message to the client.
+c.send('Thank you for connecting')
+m = c.recv(70)
+
+input = pickle.loads(m)
+
+M_R = int(input[0])
+M_L = int(input[1])
+
+print M_R
+print M_L
+
+
+# Close the connection with the client
+c.close()
