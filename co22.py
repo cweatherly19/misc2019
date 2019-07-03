a2cdd5ed6062fa36088b356f4c4d13671b6e6fab
Your Name <abackes19@bsmschool.org>
2019-07-04 08:16:03 +1000

    co2 and sensors work

--- co22.py
+++ co22.py
@@ -11,13 +11,25 @@
 import RoboPiLib as RPL
 import setup
 import post_to_web as PTW
-import time
+import time, math
 
 pi = True
-co = 0
+co = 6
 average = [ ]
 base = 0
 content = 0
+
+
+frontr = RPL.analogRead(fr)
+frontl = RPL.analogRead(fl)
+sidel = RPL.analogRead(sl)
+sider = RPL.analogRead(sr)
+
+
+fr = 2
+fl = 1
+sl = 0
+sr = 3
 
 start = time.time()
 now = time.time()
@@ -36,6 +48,14 @@ except:
 while now - start < 1080:
     if pi == True:
         content = RPL.analogRead(co)
+
+        frontr = RPL.analogRead(fr)
+        frontl = RPL.analogRead(fl)
+        sidel = RPL.analogRead(sl)
+        sider = RPL.analogRead(sr)
+
+
+
     else:
         content = 357
     if base - content >= 30:
@@ -46,12 +66,11 @@ while now - start < 1080:
 
     else:
         PTW.state['CO2detect: No life detected'] = 1
-        
+
     PTW.state['Co2 data: '] = content
     now = time.time()
     minutes, seconds = divmod((now - start), 60)
     PTW.state['Minutes: '] = minutes
-    PTW.state['Seconds: '] = seconds
+    PTW.state['Seconds: '] = round(seconds, 1)
 
     PTW.post()
-
